0e7e64b317f449c999623381d07bf52dd66450c1
ayush <ankithelpadi143ayush@gmail.com>
2023-09-26 17:22:53 +0530

    improved profile

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -27,30 +27,30 @@ function Profile() {
           </span>
         </div>
         <div className="flex flex-wrap">
-        <div className="no flex  m-7 space-x-5">
-          <p className="projets">
-            <p className=" font-bold md:text-lg lg:text-2xl">113</p>
-            <p>Porjects</p>
-          </p>
-          <p className="Followers">
-            <p className=" font-bold md:text-lg lg:text-2xl">12.2K</p>
-            <p>Followers</p>
-          </p>
-          <p className="Folling">
-            <p className=" font-bold md:text-lg lg:text-2xl"> 20k</p>
-            <p>Following</p>
-          </p>
+          <div className="no flex  m-7 space-x-5">
+            <p className="projets">
+              <p className=" font-bold md:text-lg lg:text-2xl">113</p>
+              <p>Porjects</p>
+            </p>
+            <p className="Followers">
+              <p className=" font-bold md:text-lg lg:text-2xl">12.2K</p>
+              <p>Followers</p>
+            </p>
+            <p className="Folling">
+              <p className=" font-bold md:text-lg lg:text-2xl"> 20k</p>
+              <p>Following</p>
+            </p>
+          </div>
+          <div className="btn m-7">
+            <button className=" border-2 p-3  border-gray-400 rounded-xl shadow-inner  text-white  bg-blue-400 ">
+              + Follow
+            </button>
+          </div>
         </div>
-        <div className="btn m-7">
-          <button className=" border-2 p-3  border-gray-400 rounded-xl shadow-inner  text-white  bg-blue-400 ">
-            + Follow
-          </button>
-          </div>
-          </div>
-          
       </div>
 
-      <div className=" w-11/12 shadow-inner border-2 m-auto border-gray-300 mt-10">
+      <div className="status   ">
+      <div  className="  profile_st shadow-inner border-2  border-gray-300 mt-10">
         <div className=" m-4">
           <p>Profile Status:</p>
           <p>
@@ -143,109 +143,181 @@ function Profile() {
           </div>
         </div>
       </div>
-      <div className=" ml-7 w-11/12 flex flex-wrap justify-around  h-auto p-5 space-y-2 shadow-inner mt-10 border-gray-300  mb-6 border-2 ">
-       
-        <button className=" rounded-2xl p-1 h-12 m-auto w-36 bg-violet-500 hover:bg-violet-600"> Activity </button>
-        <button className=" rounded-2xl p-1 h-12 m-auto w-36 bg-violet-500 hover:bg-violet-600"> Activity </button>
-        <button className=" rounded-2xl p-1 h-12 m-auto w-36 bg-violet-500 hover:bg-violet-600"> Activity </button>
-        <button className=" rounded-2xl p-1 h-12 m-auto w-36 bg-violet-500 hover:bg-violet-600"> Activity </button>
-    
-      </div>
-
-      <div className="stack w-11/12 mb-10 shadow-inner ml-7 ">
-
-      <ul  class="divide-y divide-gray-100">
-  <li class="flex justify-between gap-x-6 py-5">
-    <div class="flex min-w-0 gap-x-4">
-      <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-      <div class="min-w-0 flex-auto">
-        <p class="text-sm font-semibold leading-6 text-gray-900">Leslie Alexander</p>
-        <p class="mt-1 truncate text-xs leading-5 text-gray-500">leslie.alexander@example.com</p>
-      </div>
-    </div>
-    <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-      <p class="text-sm leading-6 text-gray-900">Co-Founder / CEO</p>
-      <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p>
-    </div>
-  </li>
-  <li class="flex justify-between gap-x-6 py-5">
-    <div class="flex min-w-0 gap-x-4">
-      <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-      <div class="min-w-0 flex-auto">
-        <p class="text-sm font-semibold leading-6 text-gray-900">Michael Foster</p>
-        <p class="mt-1 truncate text-xs leading-5 text-gray-500">michael.foster@example.com</p>
-      </div>
-    </div>
-    <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-      <p class="text-sm leading-6 text-gray-900">Co-Founder / CTO</p>
-      <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p>
-    </div>
-  </li>
-  <li class="flex justify-between gap-x-6 py-5">
-    <div class="flex min-w-0 gap-x-4">
-      <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-      <div class="min-w-0 flex-auto">
-        <p class="text-sm font-semibold leading-6 text-gray-900">Dries Vincent</p>
-        <p class="mt-1 truncate text-xs leading-5 text-gray-500">dries.vincent@example.com</p>
-      </div>
-    </div>
-    <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-      <p class="text-sm leading-6 text-gray-900">Business Relations</p>
-      <div class="mt-1 flex items-center gap-x-1.5">
-        <div class="flex-none rounded-full bg-emerald-500/20 p-1">
-          <div class="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
+      <div className="r_cont  ">
+        <div className="m-auto w-11/12 flex flex-wrap justify-around  h-auto p-5 space-y-2 shadow-inner mt-10 border-gray-300  mb-6 border-2 ">
+          <button className=" rounded-2xl p-1 h-12 m-auto w-36 bg-violet-500 hover:bg-violet-600">
+            {" "}
+            Activity{" "}
+          </button>
+          <button className=" rounded-2xl p-1 h-12 m-auto w-36 bg-violet-500 hover:bg-violet-600">
+            {" "}
+            Activity{" "}
+          </button>
+          <button className=" rounded-2xl p-1 h-12 m-auto w-36 bg-violet-500 hover:bg-violet-600">
+            {" "}
+            Activity{" "}
+          </button>
+          <button className=" rounded-2xl p-1 h-12 m-auto w-36 bg-violet-500 hover:bg-violet-600">
+            {" "}
+            Activity{" "}
+          </button>
         </div>
-        <p class="text-xs leading-5 text-gray-500">Online</p>
-      </div>
-    </div>
-  </li>
-  <li class="flex justify-between gap-x-6 py-5">
-    <div class="flex min-w-0 gap-x-4">
-      <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-      <div class="min-w-0 flex-auto">
-        <p class="text-sm font-semibold leading-6 text-gray-900">Lindsay Walton</p>
-        <p class="mt-1 truncate text-xs leading-5 text-gray-500">lindsay.walton@example.com</p>
-      </div>
-    </div>
-    <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-      <p class="text-sm leading-6 text-gray-900">Front-end Developer</p>
-      <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p>
-    </div>
-  </li>
-  <li class="flex justify-between gap-x-6 py-5">
-    <div class="flex min-w-0 gap-x-4">
-      <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-      <div class="min-w-0 flex-auto">
-        <p class="text-sm font-semibold leading-6 text-gray-900">Courtney Henry</p>
-        <p class="mt-1 truncate text-xs leading-5 text-gray-500">courtney.henry@example.com</p>
-      </div>
-    </div>
-    <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-      <p class="text-sm leading-6 text-gray-900">Designer</p>
-      <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p>
-    </div>
-  </li>
-  <li class="flex justify-between gap-x-6 py-5">
-    <div class="flex min-w-0 gap-x-4">
-      <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-      <div class="min-w-0 flex-auto">
-        <p class="text-sm font-semibold leading-6 text-gray-900">Tom Cook</p>
-        <p class="mt-1 truncate text-xs leading-5 text-gray-500">tom.cook@example.com</p>
-      </div>
-    </div>
-    <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-      <p class="text-sm leading-6 text-gray-900">Director of Product</p>
-      <div class="mt-1 flex items-center gap-x-1.5">
-        <div class="flex-none rounded-full bg-emerald-500/20 p-1">
-          <div class="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
+
+        <div className="stack w-11/12 mb-10 shadow-inner m-auto overflow-x-auto customize_overflow ">
+          <ul class="divide-y divide-gray-100">
+            <li class="flex justify-between gap-x-6 py-5">
+              <div class="flex min-w-0 gap-x-4">
+                <img
+                  class="h-12 w-12 flex-none rounded-full bg-gray-50"
+                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="min-w-0 flex-auto">
+                  <p class="text-sm font-semibold leading-6 text-gray-900">
+                    Leslie Alexander
+                  </p>
+                  <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+                    leslie.alexander@example.com
+                  </p>
+                </div>
+              </div>
+              <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                <p class="text-sm leading-6 text-gray-900">Co-Founder / CEO</p>
+                <p class="mt-1 text-xs leading-5 text-gray-500">
+                  Last seen <time datetime="2023-01-23T13:23Z">3h ago</time>
+                </p>
+              </div>
+            </li>
+            <li class="flex justify-between gap-x-6 py-5">
+              <div class="flex min-w-0 gap-x-4">
+                <img
+                  class="h-12 w-12 flex-none rounded-full bg-gray-50"
+                  src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="min-w-0 flex-auto">
+                  <p class="text-sm font-semibold leading-6 text-gray-900">
+                    Michael Foster
+                  </p>
+                  <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+                    michael.foster@example.com
+                  </p>
+                </div>
+              </div>
+              <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                <p class="text-sm leading-6 text-gray-900">Co-Founder / CTO</p>
+                <p class="mt-1 text-xs leading-5 text-gray-500">
+                  Last seen <time datetime="2023-01-23T13:23Z">3h ago</time>
+                </p>
+              </div>
+            </li>
+            <li class="flex justify-between gap-x-6 py-5">
+              <div class="flex min-w-0 gap-x-4">
+                <img
+                  class="h-12 w-12 flex-none rounded-full bg-gray-50"
+                  src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="min-w-0 flex-auto">
+                  <p class="text-sm font-semibold leading-6 text-gray-900">
+                    Dries Vincent
+                  </p>
+                  <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+                    dries.vincent@example.com
+                  </p>
+                </div>
+              </div>
+              <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                <p class="text-sm leading-6 text-gray-900">
+                  Business Relations
+                </p>
+                <div class="mt-1 flex items-center gap-x-1.5">
+                  <div class="flex-none rounded-full bg-emerald-500/20 p-1">
+                    <div class="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
+                  </div>
+                  <p class="text-xs leading-5 text-gray-500">Online</p>
+                </div>
+              </div>
+            </li>
+            <li class="flex justify-between gap-x-6 py-5">
+              <div class="flex min-w-0 gap-x-4">
+                <img
+                  class="h-12 w-12 flex-none rounded-full bg-gray-50"
+                  src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="min-w-0 flex-auto">
+                  <p class="text-sm font-semibold leading-6 text-gray-900">
+                    Lindsay Walton
+                  </p>
+                  <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+                    lindsay.walton@example.com
+                  </p>
+                </div>
+              </div>
+              <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                <p class="text-sm leading-6 text-gray-900">
+                  Front-end Developer
+                </p>
+                <p class="mt-1 text-xs leading-5 text-gray-500">
+                  Last seen <time datetime="2023-01-23T13:23Z">3h ago</time>
+                </p>
+              </div>
+            </li>
+            <li class="flex justify-between gap-x-6 py-5">
+              <div class="flex min-w-0 gap-x-4">
+                <img
+                  class="h-12 w-12 flex-none rounded-full bg-gray-50"
+                  src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="min-w-0 flex-auto">
+                  <p class="text-sm font-semibold leading-6 text-gray-900">
+                    Courtney Henry
+                  </p>
+                  <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+                    courtney.henry@example.com
+                  </p>
+                </div>
+              </div>
+              <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                <p class="text-sm leading-6 text-gray-900">Designer</p>
+                <p class="mt-1 text-xs leading-5 text-gray-500">
+                  Last seen <time datetime="2023-01-23T13:23Z">3h ago</time>
+                </p>
+              </div>
+            </li>
+            <li class="flex justify-between gap-x-6 py-5">
+              <div class="flex min-w-0 gap-x-4">
+                <img
+                  class="h-12 w-12 flex-none rounded-full bg-gray-50"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="min-w-0 flex-auto">
+                  <p class="text-sm font-semibold leading-6 text-gray-900">
+                    Tom Cook
+                  </p>
+                  <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+                    tom.cook@example.com
+                  </p>
+                </div>
+              </div>
+              <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                <p class="text-sm leading-6 text-gray-900">
+                  Director of Product
+                </p>
+                <div class="mt-1 flex items-center gap-x-1.5">
+                  <div class="flex-none rounded-full bg-emerald-500/20 p-1">
+                    <div class="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
+                  </div>
+                  <p class="text-xs leading-5 text-gray-500">Online</p>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
-        <p class="text-xs leading-5 text-gray-500">Online</p>
-      </div>
-    </div>
-  </li>
-</ul>
-
-
+        </div>
       </div>
     </>
   );
